42766ed9daf94ed386d99780692f63e0cd5828c5
Tanpreet <tanpreetjhally300@gmail.com>
2026-07-13 10:34:36 +0530

    scripts added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 import NavbarAndFooterProvider from "@/components/NavbarAndFooterProvider";
 import { UserProvider } from "@/context/UserContext";
 import WhatsappButton from "@/components/WhatsappButton";
@@ -25,6 +26,25 @@ export default function RootLayout({
       <body
         className={`${inter.variable} antialiased`}
       >
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T4WNPSBN"
+          height="0" width="0" style={{ display: "none", visibility: "hidden" }}>
+          </iframe>
+        </noscript>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T4WNPSBN');
+          `}
+        </Script>
         <UserProvider>
         <NavbarAndFooterProvider>
         {children}
